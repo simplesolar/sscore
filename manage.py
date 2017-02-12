5a292c8286fd38962fc608e471c89c2044a70063
Awili Uzo <uzo.awili@andela.com>
2017-02-12 09:28:25 +0100

    fix BASE_DIR setting

--- manage.py
+++ manage.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 import os
 import sys
+
 import dotenv
 
 if __name__ == "__main__":
-
     dotenv.read_dotenv()
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sscore.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sscore.settings.local")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
